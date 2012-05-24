1117508a27030f74fea43afa1e9375b09cf14ff4
Pierre-Guy Lempereur <la101576@student.helha.be>
2012-05-24 21:50:17 +0200

    FIX Gôôgle books Récupération de la cover si disponible

--- Autocomplete/ac-book.js
+++ Autocomplete/ac-book.js
@@ -1,5 +1,5 @@
 var uri = "https://www.googleapis.com/books/v1/volumes";
-var append = "&key=AIzaSyACxun2j0Wat4J9vtrBFDSgluDft7UI-mg&callback=?";
+var append = "key=AIzaSyACxun2j0Wat4J9vtrBFDSgluDft7UI-mg&callback=?";
 
 function getBooks(lang)
 {
@@ -17,13 +17,14 @@ function addBook(book)
         "<li>Authors : " + authorstr + "</li>" +
         "<li>Title : " + book.title + "</li>" +
         "<li>Plot : " + book.plot + "</li>" +
+        "<li>Cover : <img width=150 src=\"" + book.cover + "\"/></li>" +
         "</ul>"
     );
 }
 
 function searchGBook(name, lang)
 {
-    var url = uri + "?q=" + name + "&langRestrict=" + lang + append;
+    var url = uri + "?q=" + name + "&langRestrict=" + lang + "&" + append;
     
     $.ajax({
         type:"GET",
@@ -42,10 +43,15 @@ function searchGBook(name, lang)
                     for (var j=0; j<data.items[i].volumeInfo.authors.length; j++)
                         authors.push({"name":data.items[i].volumeInfo.authors[j]});
                     
+                    var image = "";
+                    if (data.items[i].volumeInfo.imageLinks != undefined)
+                        image = data.items[i].volumeInfo.imageLinks["thumbnail"];
+                    
                     var book = {
                         authors: authors,
                         title: data.items[i].volumeInfo.title,
                         plot: data.items[i].volumeInfo.description,
+                        cover: image,
                         publisher: data.items[i].volumeInfo.publisher,
                         released: data.items[i].volumeInfo.publishedDate,
                         pageCount: data.items[i].volumeInfo.pageCount
